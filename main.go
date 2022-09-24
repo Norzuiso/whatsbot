d96575ee041c1974d96696815426a84e204d1dea
Josue Aguilar <Norzuiso>
2022-09-24 01:46:29 -0500

    created response messages

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 	"syscall"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal"
 	"go.mau.fi/whatsmeow"
 	waProto "go.mau.fi/whatsmeow/binary/proto"
+	"go.mau.fi/whatsmeow/store/sqlstore"
 	"go.mau.fi/whatsmeow/types/events"
-	"google.golang.org/protobuf/proto"
+	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
 type MyClient struct {
@@ -19,30 +21,45 @@ type MyClient struct {
 	eventHandlerID uint32
 }
 
-func (mycli *MyClient) register() {
-	mycli.eventHandlerID = mycli.WAClient.AddEventHandler(mycli.myEventHandler)
-}
-
-func (mycli *MyClient) myEventHandler(evt interface{}) {
-	switch v := evt.(type) {
-	case *events.Message:
-		fmt.Println("Received a message!", v.Message.GetConversation())
-		/*sender := v.Info.MessageSource.Sender
-				cli.SendMessage(context.Background(), targetJID, "", &waProto.Message{
-		    		Conversation: proto.String("Hello, World!"),
-				})
-		*/
-		mycli.WAClient.SendMessage(context.Background(), v.Info.MessageSource.Sender, "", &waProto.Message{
-			Conversation: proto.String("Hello, World!"),
-		})
-	case *events.Receipt:
-		fmt.Println("Received a receipt!", v.Chat.User)
+func WAConnect() (*whatsmeow.Client, error) {
+	dbLog := waLog.Stdout("Database", "DEBUG", true)
+	container, err := sqlstore.New("sqlite3", "file:wapp.db?_foreing_key=on", dbLog)
+	if err != nil {
+		return nil, err
 	}
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+	client := whatsmeow.NewClient(deviceStore, waLog.Noop)
+	if client.Store.ID == nil {
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			return nil, err
+		}
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+			} else {
+				fmt.Println("Login event:", evt.Event)
+			}
+		}
+	} else {
+		err := client.Connect()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return client, nil
 }
 
 func main() {
 
 	cli, err := WAConnect()
+	myCli := MyClient{}
+	myCli.WAClient = cli
+	myCli.register()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -52,5 +69,28 @@ func main() {
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
 
-	cli.WAClient.Disconnect()
+	myCli.WAClient.Disconnect()
+}
+
+func (mycli *MyClient) register() {
+	mycli.eventHandlerID = mycli.WAClient.AddEventHandler(mycli.myEventHandler)
+}
+
+func (mycli *MyClient) myEventHandler(evt interface{}) {
+	switch v := evt.(type) {
+	case *events.Message:
+		message := v.Message.GetConversation()
+		fmt.Println(message)
+		if message != "" && len(message) != 0 {
+
+			x := "Holi"
+			mycli.WAClient.SendMessage(context.TODO(), v.Info.MessageSource.Chat, "", &waProto.Message{
+				Conversation: &x,
+			})
+			fmt.Println("Se debio de imprimir")
+			fmt.Println(v.Info.MessageSource.Chat)
+		}
+	case *events.Receipt:
+		fmt.Println("Received a receipt!", v.Chat.User)
+	}
 }
